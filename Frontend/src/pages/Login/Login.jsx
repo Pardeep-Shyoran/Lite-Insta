bf0da3681901a29d535932a2c19b8f3de5e58d09
Pardeep Shyoran <pardeep75889@gmail.com>
2025-09-05 22:06:11 +0530

    login created

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,4 @@
 import { Helmet } from "react-helmet"
-<<<<<<< HEAD
-
-const Login = () => {
-=======
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
@@ -14,38 +10,32 @@ const Login = () => {
     console.log(data);
     reset();
   }
-  
->>>>>>> affe6b7 (project added)
+
   return (
     <>
       <Helmet>
         <title>Login - Lite-Insta</title>
         <meta name="description" content="Login to Lite-Insta to access your account." />
       </Helmet>
-<<<<<<< HEAD
-      <div>Login</div>
-=======
       <div>
         <div>
-        <h1>Login Page</h1>
-        <form onSubmit={handleSubmit(LoginHandler)} method="POST">
-          <div>
-            <label>Username: </label>
-            <input type="text" {...register('username')} />
-          </div>
+          <h1>Login Page</h1>
+          <form onSubmit={handleSubmit(LoginHandler)} method="POST">
+            <div>
+              <label>Username: </label>
+              <input type="text" {...register('username')} />
+            </div>
 
-          <div>
-            <label>Password: </label>
-            <input type="password" {...register('password')} />
-          </div>
+            <div>
+              <label>Password: </label>
+              <input type="password" {...register('password')} />
+            </div>
 
-          <button type="submit">Login</button>
-          <p>Don't have an Account? <Link to="/register">Register</Link></p>
-
-        </form>
+            <button type="submit">Login</button>
+            <p>Don't have an Account? <Link to="/register">Register</Link></p>
+          </form>
+        </div>
       </div>
-      </div>
->>>>>>> affe6b7 (project added)
     </>
   )
 }
