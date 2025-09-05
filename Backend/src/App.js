f7893c3df44f7d49537fca628de6365294ff1f85
Pardeep Shyoran <pardeep75889@gmail.com>
2025-09-05 21:43:05 +0530

    project added

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -2,11 +2,19 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const authRoutes = require('./routes/auth.routes')
 const postRoutes = require('./routes/post.routes')
+<<<<<<< HEAD
+=======
+const cors = require('cors')
+>>>>>>> affe6b7 (project added)
 
 
 const App = express()
 
+<<<<<<< HEAD
 
+=======
+App.use(cors());
+>>>>>>> affe6b7 (project added)
 App.use(express.json())
 App.use(cookieParser())
 App.use('/api/auth', authRoutes)
--- Frontend/src/pages/Register/Register.jsx
+++ Frontend/src/pages/Register/Register.jsx
@@ -1,4 +1,8 @@
 import { Helmet } from "react-helmet";
+<<<<<<< HEAD
+=======
+import { useForm } from "react-hook-form";
+>>>>>>> affe6b7 (project added)
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -6,6 +10,23 @@ import { toast } from "react-toastify"
 import styles from "./Register.module.css";
 
 const Register = () => {
+<<<<<<< HEAD
+=======
+
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  const { register, reset, handleSubmit,} = useForm();
+
+  function RegisterHandler(data) {
+    console.log(data);
+    // Implement registration logic here, e.g., send data to backend
+    toast.success("Registered successfully!");
+    reset();
+    navigate("/login");
+  }
+
+
+>>>>>>> affe6b7 (project added)
   return (
     <>
       <Helmet>
@@ -14,7 +35,40 @@ const Register = () => {
       </Helmet>
       <div>
         <h1>Register Page</h1>
+<<<<<<< HEAD
 
+=======
+        <form onSubmit={handleSubmit(RegisterHandler)} method="POST" encType="multipart/form-data">
+          <div>
+            <label>Choose Profile Pic: </label>
+            <input type="file" {...register('profilePic')} />
+          </div>
+
+          {/* <div>
+            <label>Full Name: </label>
+            <input type="text" />
+          </div>
+
+          <div>
+            <label>Email: </label>
+            <input type="email" />
+          </div> */}
+
+          <div>
+            <label>Username: </label>
+            <input type="text" {...register('username')} />
+          </div>
+
+          <div>
+            <label>Password: </label>
+            <input type="password" {...register('password')} />
+          </div>
+
+          <button type="submit">Register</button>
+          <p>Already have an account? <Link to="/login">Login</Link></p>
+
+        </form>
+>>>>>>> affe6b7 (project added)
       </div>
     </>
   )
