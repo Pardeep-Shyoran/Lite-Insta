edc27e8016a20c5f326ab329c880f1054d3f9756
Pardeep Shyoran <pardeep75889@gmail.com>
2025-09-13 19:46:14 +0530

    some new change

--- Frontend/src/routes/MainRoutes.jsx
+++ Frontend/src/routes/MainRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
 import Register from '../pages/Register/Register'
@@ -6,13 +6,8 @@ import PageNotFound from '../pages/PageNotFound/PageNotFound';
 
 const MainRoutes = () => {
 
-    // const location = useLocation();
-
-    // const isFound = location.pathname === '/404' || !['/', '/login', '/register'].includes(location.pathname);
-
     return (
         <>
-            {/* {!isFound && <Navbar />} */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
