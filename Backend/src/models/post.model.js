00fc579e51c5ffa1f9ddab4a538502106487a9fc
Pardeep Shyoran <pardeep75889@gmail.com>
2025-10-02 13:27:39 +0530

    fix: correct syntax for postSchema definition in post model

--- Backend/src/models/post.model.js
+++ Backend/src/models/post.model.js
@@ -8,7 +8,7 @@ const postSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"user"
     }
-})
+}, { timestamps: true });
 
 
 const postModel = mongoose.model("post", postSchema);
