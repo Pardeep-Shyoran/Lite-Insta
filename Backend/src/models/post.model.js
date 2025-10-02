c5fbb045a5d8da20b5a240aa3cb856ffa518e599
Pardeep Shyoran <pardeep75889@gmail.com>
2025-10-02 23:44:49 +0530

    image creation data enhanced

--- Backend/src/models/post.model.js
+++ Backend/src/models/post.model.js
@@ -7,7 +7,8 @@ const postSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"user"
-    }
+    },
+    imageKitFileId: String,
 }, { timestamps: true });
 
 
